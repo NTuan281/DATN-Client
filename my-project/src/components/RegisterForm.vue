511c8afa50a9aed8e2ff7222aaa749b2c0b3bada
NTuan281 <113279374+NTuan281@users.noreply.github.com>
2023-12-05 15:53:17 +0700

    Change RegisterForm file

--- my-project/src/components/RegisterForm.vue
+++ my-project/src/components/RegisterForm.vue
@@ -3,46 +3,65 @@
   <div class="px-6 pb-1.5 text-[15px]">Full Name</div>
   <div class="px-6 pb-2">
     <TextInput
-      placeholder="Your name"
-      v-model:input="name"
+      placeholder="Your full name"
+      v-model:input="fullName"
       inputType="text"
       :autoFocus="true"
       :error="errors && errors.name ? errors.name[0] : ''"
     />
   </div>
-  <div class="px-6 pb-1.5 text-[15px]">Email address</div>
+  <div class="px-6 pb-1.5 text-[15px]">Email</div>
   <div class="px-6 pb-2">
     <TextInput
-      placeholder="Email address"
+      placeholder="Your email"
       v-model:input="email"
-      inputType="email"
+      inputType="text"
+      :error="errors && errors.password ? errors.password[0] : ''"
+    />
+  </div>
+  <div class="px-6 pb-1.5 text-[15px]">User Name</div>
+  <div class="px-6 pb-2">
+    <TextInput
+      placeholder="Your user name"
+      v-model:input="fullName"
+      inputType="text"
       :autoFocus="true"
-      :error="errors && errors.email ? errors.email[0] : ''"
+      :error="errors && errors.name ? errors.name[0] : ''"
     />
   </div>
   <div class="px-6 pb-1.5 text-[15px]">Password</div>
   <div class="px-6 pb-2">
     <TextInput
-      placeholder="Password"
+      placeholder="password"
       v-model:input="password"
       inputType="password"
-      :error="errors && errors.password ? errors.password[0] : ''"
+      :autoFocus="true"
+      :error="errors && errors.email ? errors.email[0] : ''"
     />
   </div>
-  <div class="px-6 pb-1.5 text-[15px]">Confirm Password</div>
+  <div class="px-6 pb-1.5 text-[15px]">Conform Password</div>
   <div class="px-6 pb-2">
     <TextInput
-      placeholder="Confirm Password"
+      placeholder="Email address"
       v-model:input="confirmPassword"
       inputType="password"
-      :error="errors && errors.confirmPassword ? errors.confirmPassword[0] : ''"
+      :autoFocus="true"
+      :error="errors && errors.email ? errors.email[0] : ''"
+    />
+  </div>
+  <div class="px-6 pb-1.5 text-[15px]">Description</div>
+  <div class="px-6 pb-2">
+    <TextInput
+      placeholder="Your description"
+      v-model:input="description" 
+      inputType="text"
     />
   </div>
 
   <div class="px-6 pb-2 mt-6">
     <button
-      :disabled="!email || !password || !confirmPassword || !name"
-      :class="!email || !password || !confirmPassword || !name ? 'bg-gray-200' : 'bg-[#f02c56]'"
+      :disabled="!userName || !password || !confirmPassword || !fullName || !email || !description"
+      :class="!userName || !password || !confirmPassword || !fullName || !email || !description ? 'bg-gray-200' : 'bg-[#f02c56]'"
       @click="register()"
       class="w-full text-[17px] font-semibold text-white py-3 rounded-sm"
     >
@@ -51,11 +70,50 @@
   </div>
 </template>
 <script setup>
-import { ref } from 'vue'
-import TextInput from './TextInput.vue'
-let email = ref(null)
-let password = ref(null)
-let confirmPassword = ref(null)
-let name = ref(null)
-let errors = ref(null)
+import { ref } from 'vue';
+import axiosClient from '../api/clientAxiosApi';
+import TextInput from "./TextInput.vue"
+
+const email = ref('');
+const password = ref('');
+const description = ref('');
+const fullName = ref('');
+const userName = ref('');
+const errors = ref('');
+const confirmPassword = ref('');
+
+
+const register = async () => {
+  // Check if any input is empty
+  if (email.value === ''){
+    errors.email.value = 'Please fill in the field.'
+  } else if(password.value === '' ){
+    errors.password.value = 'Please fill in the field.'
+  } else if(fullName.value === '') {
+    errors.fullName.value = 'Please fill in the field.'
+  } else if(userName.value === '') {
+    errors.value = 'Please fill in the field.';
+    return;
+  }else if(password.value !== confirmPassword.value){
+    errors.description.value = 'Confirm password does not match.';
+    return;
+  }
+
+  try {
+    const signUp = await axiosClient.post("auth/register",{
+    username: userName.value,
+    password: password.value,
+    fullname: fullName.value,
+    email: email.value,
+    description: description.value,
+    role: "USER",
+    createAt: new Date
+  })
+  } catch (error) {
+    console.log(error);
+  }
+  
+
+  
+};
 </script>
--- my-project/src/views/RegisterView.vue
+++ my-project/src/views/RegisterView.vue
@@ -1,130 +1,120 @@
 <template>
-    <section class="bg-gray-50 dark:bg-gray-900">
-    <!-- component -->
-<div class="bg-grey-lighter min-h-screen flex flex-col">
-            <div class="mb-4 container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-1 sm:max-w-md xl:p-8 dark:bg-gray-800 dark:border-gray-700 px-4">
-                    <h1 class="mt-4 mb-10 text-3xl text-center dark:text-white space-y-4 md:space-y-6">Sign up</h1>
-                    <input 
-                        v-model="fullName"
-                        type="text"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-grey-light rounded mb-6"
-                        name="fullname"
-                        required=""
-                        placeholder="Full Name" />
-                    <input 
-                        v-model="email" 
-                        type="email" 
-                        name="email" 
-                        id="email" 
-                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-grey-light mb-6"
-                        placeholder="name@company.com" 
-                        required=""/>
-                    <input 
-                        v-model="userName"
-                        type="text"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-grey-light rounded mb-6"
-                        name="userName"
-                        required=""
-                        placeholder="User Name"/>
-                    <input 
-                        v-model="password"
-                        type="password"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-grey-light mb-6"
-                        name="password"
-                        required=""
-                        placeholder="Password" />
-                    <input 
-                        v-model="confirmPassword"
-                        type="password"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-grey-light mb-6"
-                        name="confirmPassword"
-                        required=""
-                        placeholder="Confirm Password" />
-                    <input 
-                        v-model="description"
-                        type="text"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-grey-light mb-3"
-                        name="description"
-                        required=""
-                        placeholder="Description (e.g BE Developer, FE Developer, ....)" />
-                        <p class = "text-center text-red-500 space-y-4 md:space-y-6" v-if="error">{{ error }}</p>
-                        <button type="submit" @click="register" class="mt-3 w-full text-white bg-primary-600 border hover:bg-gray-300 duration-300  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                    <div class="text-center text-sm dark:text-white mt-4 mb-4">
-                        By signing up, you agree to the 
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
-                            Terms of Service
-                        </a> and 
-                        <a class="no-underline border-b border-grey-dark text-white" href="#">
-                            Privacy Policy
-                        </a>
-                    </div>
-                </div>
-
-                <div class="text-grey-dark mt-6 text-white">
-                    Already have an account? 
-                    <a class="no-underline border-b border-blue text-white" href="../login/">
-                        Log in
-                    </a>.
-                </div>
-            </div>
-        </div>
-    </section>
-</template>
-<script setup>
-import { ref } from 'vue';
-import axiosClient from '../api/clientAxiosApi';
-// import bcrypt from 'bcryptjs';
-
-const email = ref('');
-const password = ref('');
-const description = ref('');
-const fullName = ref('');
-const userName = ref('');
-const error = ref('');
-const confirmPassword = ref('');
-
-
-const register = async () => {
-  // Check if any input is empty
-  if (
-    email.value === '' ||
-    password.value === '' ||
-    description.value === '' ||
-    fullName.value === '' ||
-    userName.value === ''
-  ) {
-    error.value = 'Please fill in all fields.';
-    return;
-  }else if(password.value !== confirmPassword.value){
-    error.value = 'Confirm password does not match.';
-    return;
-  }
-  // Hash the password on the client side
-//   const hashedPassword = await bcrypt.hash(password.value, 10);
-
-  try {
-    const response = await axiosClient.post('users', {
-      userName: userName.value,
+    <div class="text-center text-[28px] mb-4 font-bold">Register</div>
+    <div class="px-6 pb-1.5 text-[15px]">Full Name</div>
+    <div class="px-6 pb-2">
+      <TextInput
+        placeholder="Your full name"
+        v-model:input="fullName"
+        inputType="text"
+        :autoFocus="true"
+        :error="errors && errors.name ? errors.name[0] : ''"
+      />
+    </div>
+    <div class="px-6 pb-1.5 text-[15px]">Email</div>
+    <div class="px-6 pb-2">
+      <TextInput
+        placeholder="Your email"
+        v-model:input="email"
+        inputType="text"
+        :error="errors && errors.password ? errors.password[0] : ''"
+      />
+    </div>
+    <div class="px-6 pb-1.5 text-[15px]">User Name</div>
+    <div class="px-6 pb-2">
+      <TextInput
+        placeholder="Your user name"
+        v-model:input="fullName"
+        inputType="text"
+        :autoFocus="true"
+        :error="errors && errors.name ? errors.name[0] : ''"
+      />
+    </div>
+    <div class="px-6 pb-1.5 text-[15px]">Password</div>
+    <div class="px-6 pb-2">
+      <TextInput
+        placeholder="password"
+        v-model:input="password"
+        inputType="password"
+        :autoFocus="true"
+        :error="errors && errors.email ? errors.email[0] : ''"
+      />
+    </div>
+    <div class="px-6 pb-1.5 text-[15px]">Conform Password</div>
+    <div class="px-6 pb-2">
+      <TextInput
+        placeholder="Email address"
+        v-model:input="confirmPassword"
+        inputType="password"
+        :autoFocus="true"
+        :error="errors && errors.email ? errors.email[0] : ''"
+      />
+    </div>
+    <div class="px-6 pb-1.5 text-[15px]">Description</div>
+    <div class="px-6 pb-2">
+      <TextInput
+        placeholder="Your description"
+        v-model:input="description" 
+        inputType="text"
+      />
+    </div>
+  
+    <div class="px-6 pb-2 mt-6">
+      <button
+        :disabled="!userName || !password || !confirmPassword || !fullName || !email || !description"
+        :class="!userName || !password || !confirmPassword || !fullName || !email || !description ? 'bg-gray-200' : 'bg-[#f02c56]'"
+        @click="register()"
+        class="w-full text-[17px] font-semibold text-white py-3 rounded-sm"
+      >
+        Sign up
+      </button>
+    </div>
+  </template>
+  <script setup>
+  import { ref } from 'vue';
+  import axiosClient from '../api/clientAxiosApi';
+  import TextInput from "./TextInput.vue"
+  
+  const email = ref('');
+  const password = ref('');
+  const description = ref('');
+  const fullName = ref('');
+  const userName = ref('');
+  const errors = ref('');
+  const confirmPassword = ref('');
+  
+  
+  const register = async () => {
+    // Check if any input is empty
+    if (email.value === ''){
+      errors.email.value = 'Please fill in the field.'
+    } else if(password.value === '' ){
+      errors.password.value = 'Please fill in the field.'
+    } else if(fullName.value === '') {
+      errors.fullName.value = 'Please fill in the field.'
+    } else if(userName.value === '') {
+      errors.value = 'Please fill in the field.';
+      return;
+    }else if(password.value !== confirmPassword.value){
+      errors.description.value = 'Confirm password does not match.';
+      return;
+    }
+  
+    try {
+      const signUp = await axiosClient.post("auth/register",{
+      username: userName.value,
       password: password.value,
-      description: description.value,
-      role: 'user',
+      fullname: fullName.value,
       email: email.value,
-      fullName: fullName.value,
-      createAt: new Date(),
-    });
-
-    // Handle the response if needed
-    console.log('Registration successful');
-
-    // Clear the error message
-    error.value = '';
-  } catch (error) {
-    // Handle the error if the registration fails
-    console.error('Registration failed', error);
+      description: description.value,
+      role: "USER",
+      createAt: new Date
+    })
+    } catch (error) {
+      console.log(error);
+    }
     
-    // Set the error message
-    error.value = 'Registration failed. Please try again.';
-  }
-};
-</script>
+  
+    
+  };
+  </script>
+  
